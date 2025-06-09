eba845ef30ef5247f24ed87557fbf309aee58a12
Sebastian Diaz <A01424712@tec.mx>
2025-06-09 12:19:02 -0600

    Revision

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from episodio import Episodio
 from serie import Serie
 from temporada import Temporada
 
-plataforma1 = Plataforma("SEBASDIOR CALVO")
+plataforma1 = Plataforma("MAXSTREAM")
 
-pelicula1 = Pelicula(101, "EL AUTISMO DE DIEGO GARCIA", 1.7, "Realidad")
-pelicula2 = Pelicula(102, "El pelo de Dior", 2.8, "Ciencia Ficción")
+pelicula1 = Pelicula(101, "El vengador", 1.7, "Realidad")
+pelicula2 = Pelicula(102, "Sev7n", 2.8, "Ciencia Ficción")
 
 pelicula1.agregar_Calif(5.0)
 pelicula1.agregar_Calif(4.5)
@@ -22,10 +22,13 @@ plataforma1.agregar_video(pelicula2)
 
 serie1 = Serie(200, "Suits", 500, "Drama") 
 temporada1=Temporada(1)
-episodio1=Episodio(212,"Harvey Spectre el cabron", 0.5,1)
+episodio1=Episodio(212,"Harvey Spectre, el comiezo", 0.5,1)
+episodio2=Episodio(213,"El legado del mal",0.55,2)
 episodio1.agregar_Calif(5.0)
+episodio2.agregar_Calif(3.0)
 serie1.agregar_Calif(5.0)
 temporada1.agregar_Episodio(episodio1)
+temporada1.agregar_Episodio(episodio2)
 serie1.agregar_Temporada(temporada1)
 plataforma1.agregar_video(serie1)
 
